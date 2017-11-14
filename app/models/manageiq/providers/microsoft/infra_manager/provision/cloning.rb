bca18893881f7608d86d17f3d87f2e0aea5570c0
Adam Grare <agrare@redhat.com>
2017-11-14 12:36:46 -0500

    Merge pull request #44 from cbolz/master
    missing VMMServer parameter
    (cherry picked from commit 3f248b9610e9e733b0b6bbc9b3a7698c3dd99bb6)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1513628

--- app/models/manageiq/providers/microsoft/infra_manager/provision/cloning.rb
+++ app/models/manageiq/providers/microsoft/infra_manager/provision/cloning.rb
@@ -169,7 +169,7 @@ module ManageIQ::Providers::Microsoft::InfraManager::Provision::Cloning
     <<-PS_SCRIPT
       Import-Module VirtualMachineManager | Out-Null; \
 
-      $template = Get-SCVMTemplate -Name '#{source.name}'; \
+      $template = Get-SCVMTemplate -Name '#{source.name}' -VMMServer localhost; \
       $vmconfig = New-SCVMConfiguration -VMTemplate $template -Name 'ManageIQConfig-#{dest_name}'; \
       $vmhost   = Get-SCVMHost -ComputerName '#{dest_host}'; \
 
