0627a3589e1b77ad3b5bd685982e8b9da0216c37
Gilles Dubreuil <gilles@redhat.com>
2018-02-09 13:07:24 +1100

    Autoload not needed

--- lib/manageiq-providers-scvmm.rb
+++ lib/manageiq-providers-scvmm.rb
@@ -1,10 +1,2 @@
 require "manageiq/providers/scvmm/engine"
 require "manageiq/providers/scvmm/version"
-
-module ManageIQ
-  module Providers
-    module Microsoft
-      autoload :Discovery, 'manageiq/providers/microsoft/discovery'
-    end
-  end
-end
