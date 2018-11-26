b35bb5624c39b4afdff96fe58c56ade6bd478475
Adam Grare <agrare@redhat.com>
2018-11-26 14:39:56 -0500

    Fix missing supports features for guest operations
    There were missing supports features for reboot and shutdown guest
    operations.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1650506

--- app/models/manageiq/providers/microsoft/infra_manager/vm.rb
+++ app/models/manageiq/providers/microsoft/infra_manager/vm.rb
@@ -4,6 +4,8 @@ class ManageIQ::Providers::Microsoft::InfraManager::Vm < ManageIQ::Providers::In
   supports_not :migrate, :reason => _("Migrate operation is not supported.")
   supports_not :publish
 
+  supports :reboot_guest
+  supports :shutdown_guest
   supports :reset do
     unsupported_reason_add(:reset, _('The VM is not powered on')) unless current_state == 'on'
   end
