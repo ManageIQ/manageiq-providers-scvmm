8f3e51bd6345ee74a04aec692b39ca82110ac171
Oleg Barenboim <chessbyte@gmail.com>
2019-11-14 10:28:27 -0500

    Move SCVMM-specific part of vm_scan_spec.rb from the core repo to this one.

---
+++ spec/models/manageiq/providers/microsoft/infra_manager/vm_scan_spec.rb
@@ -0,0 +1,20 @@
+describe VmScan do
+  context "A single VM Scan Job on Microsoft provider" do
+    let(:vm) do
+      vm = double("ManageIQ::Providers::Microsoft::InfraManager::Vm")
+      allow(vm).to receive(:kind_of?).with(ManageIQ::Providers::Openstack::CloudManager::Vm).and_return(false)
+      allow(vm).to receive(:kind_of?).with(ManageIQ::Providers::Microsoft::InfraManager::Vm).and_return(true)
+      vm
+    end
+    let(:job) { VmScan.new(:context => {}, :options => {}) }
+
+    describe "#call_snapshot_create" do
+      it "executes VmScan#create_snapshot and send signal :snapshot_complete" do
+        allow(VmOrTemplate).to receive(:find).and_return(vm)
+        expect(job).to receive(:create_snapshot).and_return(true)
+        expect(job).to receive(:signal).with(:snapshot_complete)
+        job.call_snapshot_create
+      end
+    end
+  end
+end
